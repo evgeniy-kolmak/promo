37ba0afe6260902929bafc8e0b558991125ef1a0
Evgeniy Kolmak <evgeniy.kolmak@gmail.com>
2022-04-12 19:11:02 +0300

    fix validate form

--- js/main.js
+++ js/main.js
@@ -53,11 +53,15 @@ $('.contacts-form').validate({
     user_name: {
       required: "Пожалуйста, заполните это поле",
       minlength: "Имя не менее 2 символов",
+      minlength: "Имя не более 11 символов",
     },
 
     user_phone: {
       required: "Пожалуйста, заполните это поле",
       minlength: "Телефон не менее 11 символов",
+      maxlength: "Поле содержит более 12 символов",
+      number: "Некорректный ввод (только цифры)"
+
     },
   },
   submitHandler: function (form) {
